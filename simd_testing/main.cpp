6b10f177ec1cb7c8dbfe028f452cc15ace4168ae
FoxMoss <foxmoss@mediaology.com>
2024-09-06 22:38:54 -0500

    Re-implement SIMD

--- simd_testing/main.cpp
+++ simd_testing/main.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <cstdint>
 #include <cstdio>
+#include <immintrin.h>
 #include <vector>
 
 int main() {
@@ -33,13 +34,42 @@ int main() {
   for (auto line : contents) {
     auto tokens = proccessLine(line);
 
-    for (auto iter = tokens.begin(); iter + 1 != tokens.end(); iter++) {
-      auto a_hash = word_hash(*iter);
-      auto b_hash = word_hash(*(iter + 1));
+    auto iter = tokens.begin();
+    while (1) {
+      uint index = 0;
+      union results {
+        float in_values[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+        float out_values[8];
+      } results;
+      float divisors[8] = {1, 1, 1, 1, 1, 1, 1, 1};
 
-      total += (float)chain.hash_to_children[a_hash][b_hash] /
-               chain.hash_to_child_total[a_hash];
-      count++;
+      for (; iter + 1 != tokens.end(); iter++) {
+        auto a_hash = word_hash(*iter);
+        auto b_hash = word_hash(*(iter + 1));
+
+        results.in_values[index] =
+            (float)chain.hash_to_children[a_hash][b_hash];
+        divisors[index] = (float)chain.hash_to_child_total[a_hash];
+        count++;
+        if (index >= 8)
+          break;
+      }
+
+      __m256 values_simd = _mm256_setr_ps(
+          results.in_values[0], results.in_values[1], results.in_values[2],
+          results.in_values[3], results.in_values[4], results.in_values[5],
+          results.in_values[6], results.in_values[7]);
+      __m256 divisors_simd =
+          _mm256_setr_ps(divisors[0], divisors[1], divisors[2], divisors[3],
+                         divisors[4], divisors[5], divisors[6], divisors[7]);
+      auto results_simd = _mm256_div_ps(values_simd, divisors_simd);
+
+      _mm256_storeu_ps(results.out_values, results_simd);
+      for (uint i = 0; i < 8; i++) {
+        total += results.out_values[i];
+      }
+      if (index != 7)
+        break;
     }
   }
 
